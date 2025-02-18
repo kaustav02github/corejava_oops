b2400ea92c20f605562357a3f61741a4c8088f9a
kaustav02github <kaustavhalder1007@gmail.com>
2025-02-18 16:16:16 +0530

    Threds added

---
+++ DemoThreads.java
@@ -0,0 +1,31 @@
+class A extends Thread
+{
+    public void run(){// run method of Thread class
+        for(int i=1;i<=100;i++)
+        {
+            System.out.println("Hi");
+        }
+    }
+}
+class B extends Thread
+{
+   public void run(){// run method of Thread class
+        for(int i=1;i<=100;i++)
+        {
+            System.out.println("Hello");
+        }
+    }
+}
+
+
+public class DemoThreads {
+    
+    public static void main(String[] args) {
+    A obj1=new A();
+    B obj2=new B();
+    obj1.start();
+    obj2.start();;
+    }
+
+}
+//run methods are called by start method of Thread class
